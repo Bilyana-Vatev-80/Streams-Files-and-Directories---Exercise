d483c9464a0e0190995a6877d88cb932e2526ffe
Bilyana Vatev <bilyanavatev@gmail.com>
2021-01-25 19:09:37 +0000

    commit init

--- src/GetFolderSize_8.java
+++ src/GetFolderSize_8.java
@@ -1,6 +1,6 @@
 import java.io.File;
 
-public class GetFolderSize_7 {
+public class GetFolderSize_8 {
     public static void main(String[] args) {
         String path = "C:\\Users\\bilya\\Dropbox\\My PC (DESKTOP-68H33HR)\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources";
 
